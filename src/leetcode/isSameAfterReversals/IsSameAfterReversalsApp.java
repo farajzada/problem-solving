eb0c6b61e2329bbc33ba2364e174dc1e018ab8f9
muradfarajzada <126752911+muradfarajzada@users.noreply.github.com>
2024-11-09 01:25:02 +0400

    IsSameAfterReversalsApp attached

---
+++ src/leetcode/isSameAfterReversals/IsSameAfterReversalsApp.java
@@ -0,0 +1,21 @@
+package leetcode.isSameAfterReversals;
+
+import java.util.Scanner;
+
+public class IsSameAfterReversalsApp {
+
+    public  static boolean isSameAfterReversals(int num) {
+        if (num == 0) return true;
+        if (num % 10 == 0) return false;
+
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println(isSameAfterReversals(num));
+    }
+
+}
