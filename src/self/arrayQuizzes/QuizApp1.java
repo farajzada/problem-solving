2c5e394cfc90d1087796150148a3d49ea3fed8e6
muradfarajzada <mfarajzadaa@gmail.com>
2024-11-09 01:51:47 +0400

    QuizApp1.java 1/3 attached

---
+++ src/self/arrayQuizzes/QuizApp1.java
@@ -0,0 +1,22 @@
+package self.arrayQuizzes;
+
+import java.util.Scanner;
+
+public class QuizApp1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        // -------- task 1: write a logic insert a word using Scanner --------
+        System.out.print("please enter the number : ");
+        String number = scanner.nextLine();
+        char lastCharacter = number.charAt(number.length() - 1);
+        System.out.println("the last character is " + lastCharacter);
+
+        // -------- task 2: write a logic insert a word and check if the last character is a vowel --------
+
+
+    }
+}
