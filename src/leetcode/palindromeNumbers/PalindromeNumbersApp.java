d4a3c3abee663c2a2dd2e832687a9db1d839ced8
muradfarajzada <mfarajzadaa@gmail.com>
2024-11-11 22:08:02 +0400

    PalindromeNumbersApp.java attached

--- src/leetcode/palindromeNumbers/PalindromeNumbersApp.java
+++ src/leetcode/palindromeNumbers/PalindromeNumbersApp.java
@@ -3,7 +3,6 @@ package leetcode.palindromeNumbers;
 import java.util.Scanner;
 
 public class PalindromeNumbersApp {
-
     public static boolean isPalindrome(int x) {
         if (x < 0 || x != 0 && x % 10 == 0)
             return false;
