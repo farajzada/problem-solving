6174125dffb0ded7aabcaf045cc3df496064af3c
muradfarajzada <mfarajzadaa@gmail.com>
2024-12-05 19:36:23 +0400

    MovePiecesObtainString.java attached

---
+++ src/leetCodeDailyTasks/movePiecesToObtainString/MovePiecesObtainString.java
@@ -0,0 +1,54 @@
+package leetCodeDailyTasks.movePiecesToObtainString;
+
+import java.util.Scanner;
+
+public class MovePiecesObtainString {
+    public boolean canChange(String start, String target) {
+        if (start.equals(target)) {
+            return true;
+        }
+        int waitL = 0;
+        int waitR = 0;
+
+        for (int i = 0; i < start.length(); i++) {
+            char curr = start.charAt(i);
+            char goal = target.charAt(i);
+
+            if (curr == 'R') {
+                if (waitL > 0) {
+                    return false;
+                }
+                waitR++;
+            }
+            if (goal == 'L') {
+                if (waitR > 0) {
+                    return false;
+                }
+                waitL++;
+            }
+            if (goal == 'R') {
+                if (waitR == 0) {
+                    return false;
+                }
+                waitR--;
+            }
+            if (curr == 'L') {
+                if (waitL == 0) {
+                    return false;
+                }
+                waitL--;
+            }
+        }
+
+        return waitL == 0 && waitR == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String start = scanner.nextLine();
+        String target = scanner.nextLine();
+        scanner.close();
+        MovePiecesObtainString movePiecesObtainString = new MovePiecesObtainString();
+        System.out.println(movePiecesObtainString.canChange(start, target));
+    }
+}
