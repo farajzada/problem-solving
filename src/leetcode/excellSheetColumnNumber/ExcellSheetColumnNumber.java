b0668a6f0cb1a50d816ef4c44a6d74630dcbc9e4
farajzada <farajzada@icloud.com>
2024-12-31 02:28:02 +0400

    ExcellSheetColumnNumber.java attached

---
+++ src/leetcode/excellSheetColumnNumber/ExcellSheetColumnNumber.java
@@ -0,0 +1,20 @@
+package leetcode.excellSheetColumnNumber;
+
+public class ExcellSheetColumnNumber {
+    public int titleToNumber(String columnTitle) {
+        int result = 0;
+        for (char c : columnTitle.toCharArray()) {
+            result = result * 26 + (c - 'A' + 1);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ExcellSheetColumnNumber excellSheetColumnNumber = new ExcellSheetColumnNumber();
+        System.out.println(excellSheetColumnNumber.titleToNumber("A"));
+        System.out.println(excellSheetColumnNumber.titleToNumber("AB"));
+        System.out.println(excellSheetColumnNumber.titleToNumber("ZY"));
+        System.out.println(excellSheetColumnNumber.titleToNumber("FXSHRXW"));
+
+    }
+}
